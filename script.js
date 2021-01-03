625f180bc96f6c41c6ac42b762495a56011f9fca
AubreyBennett <27aubreyb@gmail.com>
2021-01-03 10:50:02 -0500

    combining special characters and numbers

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ var upperAllowed;
 var numberAllowed;
 
 // variables
-specialAllowed = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "<", ">", "?", ":"]
+special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "<", ">", "?", ":"]
 
-lowerAllowed = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-upperAllowed = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
-numberAllowed = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
 var generateBtn = document.querySelector("#generate");
 
@@ -35,22 +35,40 @@ if (length < 8 || length > 128) {
   alert('The length is invalid.');
 }
 else {
+  var combined = [];
+
+  var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "<", ">", "?", ":"]
   var specialAllowed = confirm('Do you want special characters in your password?');
   console.log(specialAllowed);
-}
 
-if (length && specialAllowed){
-  var lowerAllowed = confirm('Do you want lowercase characters in your password?');
-  console.log(lowerAllowed);
-}
-else{
-  var upperAllowed = confirm('Do you want uppercase characters in your password?');
-  console.log(upperAllowed);
-}
-if (length && specialAllowed && upperAllowed){
+  if (specialAllowed) {
+    combined = combined.concat(specialAllowed);
+  }
+
+  var number = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var numberAllowed = confirm('Do you want numbers in your password?');
   console.log(numberAllowed);
+
+  if (numberAllowed) {
+    combined = combined.concat(specialAllowed);
+  }
+
+  var random = Math.floor(Math.random() * combined.length);
+  console.log(combined[random]);
 }
+
+// if (length && specialAllowed){
+//   var lowerAllowed = confirm('Do you want lowercase characters in your password?');
+//   console.log(lowerAllowed);
+// }
+// else{
+//   var upperAllowed = confirm('Do you want uppercase characters in your password?');
+//   console.log(upperAllowed);
+// }
+// if (length && specialAllowed && upperAllowed){
+//   var numberAllowed = confirm('Do you want numbers in your password?');
+//   console.log(numberAllowed);
+// }
 
 
 
