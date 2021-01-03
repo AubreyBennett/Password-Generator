2d5b9c3326871d8eda7b45e1499525b6d1abc98f
AubreyBennett <27aubreyb@gmail.com>
2021-01-03 10:33:18 -0500

    variables added with special characters, lowercase, uppercase, and numbers

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
 // Assignment Code
+var length;
+var specialAllowed;
+var lowerAllowed;
+var upperAllowed;
+var numberAllowed;
+
+// variables
+specialAllowed = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "<", ">", "?", ":"]
+
+lowerAllowed = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+upperAllowed = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+numberAllowed = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
 var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// Write password to the #password input
+function writePassword() {
+  // var password = generatePassword();
+  // var passwordText = document.querySelector("#password");
+
+  // passwordText.value = password;
+  
+}
 
 var length = prompt('Enter the length of the password');
 console.log(length);
@@ -35,3 +51,10 @@ if (length && specialAllowed && upperAllowed){
   var numberAllowed = confirm('Do you want numbers in your password?');
   console.log(numberAllowed);
 }
+
+
+
+
+
+
+
